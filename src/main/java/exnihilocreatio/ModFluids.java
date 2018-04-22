37ea9e71751bc869834b9e98ea46b7e5b425f15e
SirLyle <infraredhat@gmail.com>
2018-04-21 22:28:48 -0400

    Milk as a fluid, no more dependence on Tinkers

--- src/main/java/exnihilocreatio/ModFluids.java
+++ src/main/java/exnihilocreatio/ModFluids.java
@@ -1,6 +1,8 @@
 package exnihilocreatio;
 
+import exnihilocreatio.blocks.BlockFluidMilk;
 import exnihilocreatio.blocks.BlockFluidWitchwater;
+import exnihilocreatio.fluid.FluidMilk;
 import exnihilocreatio.fluid.FluidWitchWater;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -11,12 +13,21 @@ public class ModFluids {
     public static final FluidWitchWater fluidWitchwater = new FluidWitchWater();
     public static BlockFluidWitchwater blockWitchwater = new BlockFluidWitchwater();
 
+    public static FluidMilk fluidMilk;
+    public static BlockFluidMilk blockMilk;
+
     public static void init() {
         FluidRegistry.addBucketForFluid(fluidWitchwater);
+
+        // Milk
+        fluidMilk = new FluidMilk();
+        blockMilk = new BlockFluidMilk();
+        FluidRegistry.addBucketForFluid(fluidMilk);
     }
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         fluidWitchwater.initModel();
+        fluidMilk.initModel();
     }
 }
