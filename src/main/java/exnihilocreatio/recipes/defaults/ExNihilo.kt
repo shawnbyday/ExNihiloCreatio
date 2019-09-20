8d4415467d699f06e318d9146a7f8ea04dc95676
SirLyle <infraredhat@gmail.com>
2019-09-20 18:20:52 -0400

    Issue #272

--- src/main/java/exnihilocreatio/recipes/defaults/ExNihilo.kt
+++ src/main/java/exnihilocreatio/recipes/defaults/ExNihilo.kt
@@ -287,7 +287,7 @@ object ExNihilo: IRecipeDefaults {
             val wool = BlockInfo.getStateFromMeta(Blocks.WOOL, meta)
             registry.register(wool, ItemStack(Items.STRING, 3), 0, 1f, 0f)
             registry.register(wool, ItemStack(Items.STRING, 1), 0, 0.5f, 0.25f)
-            registry.register(wool, ItemStack(Items.DYE, 1, meta), 0, 1f/8f, 2f)
+            registry.register(wool, ItemStack(Items.DYE, 1, 15-meta), 0, 1f/8f, 2f)
         }
 
     }
